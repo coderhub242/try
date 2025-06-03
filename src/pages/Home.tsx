ab3b336edf35ff10d935aea5b27458487b5b671c
coderhub242 <coderhub242@gmail.com>
2025-06-04 00:01:11 +0530

    Updated some data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,7 +22,7 @@ const Home: React.FC = () => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6 }}
             >
-              Visionary Studios
+              Epic Media
             </motion.h1>
             <motion.p 
               className="text-xl md:text-2xl text-gray-200 mb-8"
